63e794f9bd0355eb256cccc94768cf08fd3e7979
Roope <roopestenvall@gmail.com>
2019-09-08 16:31:10 +0300

    Adding booklist.html and controller path

--- bookstoreproject/src/main/java/com/example/bookstoreproject/Web/BookController.java
+++ bookstoreproject/src/main/java/com/example/bookstoreproject/Web/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.bookstoreproject.domain.BookRepository;
 
@@ -13,6 +14,11 @@ public class BookController {
 	private BookRepository repository;
 	public String listBooks() {
 		return "";
+	}
+	@RequestMapping(value="/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 	
 }
