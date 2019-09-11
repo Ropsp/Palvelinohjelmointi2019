711488fe54ba3cbf3bf491f7c9977219467fe0d6
Roope <roopestenvall@gmail.com>
2019-09-11 19:41:46 +0300

    Added create and delete functionality.

--- bookstoreproject/src/main/java/com/example/bookstoreproject/Web/BookController.java
+++ bookstoreproject/src/main/java/com/example/bookstoreproject/Web/BookController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.bookstoreproject.domain.Book;
 import com.example.bookstoreproject.domain.BookRepository;
 
 @Controller
@@ -20,5 +23,19 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
-	
+	@RequestMapping(value = "/add")
+	public String addBook (Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
 }
